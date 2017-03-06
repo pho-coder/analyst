5d088e482da2d80f38147ea81034d1292c8daa43
Phoenix Zhang <phoenix.nir@gmail.com>
2017-03-06 15:31:32 +0800

    ifx

--- src/rocks/pho/stock/analyst.clj
+++ src/rocks/pho/stock/analyst.clj
@@ -23,16 +23,15 @@
         options-summary
         ""
         "Actions:"
-        "  start    Start a new server"
-        "  stop     Stop an existing server"
-        "  status   Print a server's status"
+        "  hs300s   Start a hs300s one day summary"
+        "  one      Start a stock summary"
         ""
         "Please refer to the manual page for more information."]
-       (string/join \newline)))
+       (clojure.string/join \newline)))
 
 (defn error-msg [errors]
   (str "The following errors occurred while parsing your command:\n\n"
-       (string/join \newline errors)))
+       (clojure.string/join \newline errors)))
 
 (defn exit [status msg]
   (println msg)
@@ -43,47 +42,46 @@
     (log/info component "stopped"))
   (shutdown-agents))
 
-(defn start-app [args]
-  (doseq [component (-> args
-                        mount/start-with-args
-                        :started)]
-    (log/info component "started"))
-  (log/debug env)
-  (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app))
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
-        data-path (:stock-data-path env)
-        summary-path (:stock-summary-data-path env)]
-    (when-not (nil? errors)
-      (log/error )
-      (System/exit 1))
-    (log/info "data path:" data-path)
-    (log/info "summary path:" summary-path)
-    (let [tp (:type options)]
-      (when (= tp "hs300s")
-        (log/info "type:" tp "dt:" (:dt options))
-        (while (and (not (utils/check-finish data-path (:dt options)))
-                    (<= (Integer. (utils/get-hour)) 22))
-          (log/info "check finish! NO FINISH")
-          (Thread/sleep 60000))
-        (log/info "FINISH check")
-        (let [summary (analysis/summary-one-day data-path (:dt options))]
-          (log/info "FINISH summary one day")
-          (utils/write-summary-one-day summary-path (:dt options) summary)
-          (log/info "finish write summary")))
-      (when (= tp "one")
-        (log/info "type:" tp "code:" (:code options))))))
+(defn run-hs300s
+  [data-path summary-path dt]
+  (while (and (not (utils/check-finish data-path dt))
+              (<= (Integer. (utils/get-hour)) 22))
+    (log/info "check finish! NO FINISH:" data-path dt)
+    (Thread/sleep 60000))
+  (log/info "FINISH check")
+  (let [summary (analysis/summary-one-day data-path dt)]
+    (log/info "FINISH summary one day")
+    (utils/write-summary-one-day summary-path dt summary)
+    (log/info "finish write summary")))
+
+(defn run-one
+  [data-path summary-path code dt]
+  (log/info data-path summary-path code))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
     ;; Handle help and error conditions
+    (log/info options)
     (cond
       (:help options) (exit 0 (usage summary))
       (not= (count arguments) 1) (exit 1 (usage summary))
       errors (exit 1 (error-msg errors)))
+
+    (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app))
+    (doseq [component (-> args
+                          mount/start-with-args
+                          :started)]
+      (log/info component "started"))
+    
     ;; Execute program with options
-    (case (first arguments)
-      "hs300s" (run-hs300s options)
-      "one" (run-one options)
-      (exit 1 (usage summary)))))
+    (let [data-path (:stock-data-path env)
+          summary-path (:stock-summary-data-path env)
+          dt (:dt options)]
+      (log/info "data path:" data-path)
+      (log/info "summary path:" summary-path)
+      (case (first arguments)
+        "hs300s" (run-hs300s data-path summary-path dt)
+        "one" (run-one data-path summary-path (:code options) dt)
+        (exit 1 (usage summary))))))
